a2ad387790462b31e06df2f93aef27c43491c535
Santiago <santiagomolinero698@gmail.com>
2024-09-29 19:31:49 -0300

    Se agregó el modo texto, para un mejor ingreso de valores

--- pixel.js
+++ pixel.js
@@ -1,9 +1,9 @@
-let resolucion="";
+var resolucion="";
 
 function  completarCeros(texto, cantidad) {
-	var acumulador = texto;
+	let acumulador = texto;
 
-	var diferencia = cantidad - texto.length;
+	let diferencia = cantidad - texto.length;
 	if (diferencia > 0 )
 	{
 		while(diferencia > 0)
@@ -18,7 +18,7 @@ function  completarCeros(texto, cantidad) {
 
 function decimalToBin(numero, digitos)
 {
-	var acumulador = '';
+	let acumulador = '';
 	while(numero > 1)
 	{
 		if (numero % 2 == 0)
@@ -65,7 +65,7 @@ function textoToBinario(decimalSTR, digitos)
 function separarConvertir(texto)
 {
 	texto = texto.split(' ');
-	var acumulador = "";
+	let acumulador = "";
 
 	for (let t of texto)
 	{
@@ -79,13 +79,13 @@ function mostrarBits(bitaje, dimensiones)
 {
 	if (bitaje.length ==  (dimensiones ** 2))
 	{
-		var posicion = 0;
+		let posicion = 0;
 
 		for (let f = 1; f <= dimensiones; f++)
 		{
 			for (let c = 1; c <= dimensiones; c++)
 			{
-				var celdaACT = document.getElementById('celdaD' + dimensiones + 'f' + f + 'c' + c);
+				let celdaACT = document.getElementById('celdaD' + dimensiones + 'f' + f + 'c' + c);
 				//console.log('celdaD' + dimensiones + 'f' + f + 'c' + c + ' >>> ' + celdaACT);
 				if (bitaje[posicion] == '1')
 				{
@@ -113,14 +113,14 @@ function iniciarGrilla(contenedor, dimension)
 {
 	for (let f = 1; f <= dimension; f++)
 	{
-		var filaN = document.createElement("div");
+		let filaN = document.createElement("div");
 		filaN.setAttribute('class', 'fila');
 		filaN.setAttribute('id', 'd' + dimension + 'fila' + f);
 		contenedor.appendChild(filaN);
 
 		for (let c = 1; c <= dimension; c++)
 		{
-			var celdaN = document.createElement("div");
+			let celdaN = document.createElement("div");
 			celdaN.setAttribute('class', 'celda');
 			celdaN.setAttribute('id', 'celdaD' + dimension + 'f' + f + 'c' + c);
 
@@ -135,7 +135,7 @@ function iniciarGrilla(contenedor, dimension)
 //cantidad: cantidad de opciones
 function inicializarOpciones(nombre, listadoC, cantidad){
 
-	var contenedorActual = 0;
+	let contenedorActual = 0;
 
 	//ciclo de creación de opciones
 	for (let c = 0; c < cantidad; c++)
@@ -150,13 +150,13 @@ function inicializarOpciones(nombre, listadoC, cantidad){
 			}
 		}
 
-		var selector = document.createElement("select");
+		let selector = document.createElement("select");
 		selector.setAttribute("id", nombre + "-" + String(c));
 
 		//ciclo de creación de números de la opción
 		for (let s = 0; s <= 255; s++)
 		{
-			var opcion = document.createElement("option");
+			let opcion = document.createElement("option");
 			opcion.value = String(s);
 			opcion.textContent = String(s);
 			selector.appendChild(opcion);
@@ -173,10 +173,10 @@ function cambiarModo(modo)
 	if (modo == "menu")
 	{
 		console.log("Modo menú activado");
-		var menu = document.getElementById("menu");
+		let menu = document.getElementById("menu");
 		menu.style.display = "flex";
 
-		var resultado = document.getElementById("resultado");
+		let resultado = document.getElementById("resultado");
 		resultado.style.display = "none";
 
 	}
@@ -184,20 +184,20 @@ function cambiarModo(modo)
 	if (modo == "grilla")
 	{
 		console.log("Modo grilla activado");
-		var menu = document.getElementById("menu");
+		let menu = document.getElementById("menu");
 		menu.style.display = "none";
 
-		var resultado = document.getElementById("resultado");
+		let resultado = document.getElementById("resultado");
 		resultado.style.display = "flex";
 
 		if (resolucion == "8x8")
 		{
 			console.log("Mostrar 8x8");
-			var grilla8 = document.getElementById("grilla8");
+			let grilla8 = document.getElementById("grilla8");
 			console.log("grilla8: ", grilla8);
 			grilla8.style.display = "flex";
 
-			var grilla16 = document.getElementById("grilla16");
+			let grilla16 = document.getElementById("grilla16");
 			console.log("grilla16: ", grilla16);
 			grilla16.style.display = "none";
 		}
@@ -205,10 +205,10 @@ function cambiarModo(modo)
 		if (resolucion == "16x16")
 		{
 			console.log("Mostrar 16x16");
-			var grilla8 = document.getElementById("grilla8");
+			let grilla8 = document.getElementById("grilla8");
 			grilla8.style.display = "none";
 
-			var grilla16 = document.getElementById("grilla16");
+			let grilla16 = document.getElementById("grilla16");
 			grilla16.style.display = "flex";
 		}		
 	}
@@ -218,11 +218,11 @@ function dibujarPixeles()
 {
 	if (resolucion == "8x8")
 	{
-		var acumulador = "";
+		let acumulador = "";
 
 		for (let x = 0; x < 8; x++)
 		{
-			var numero = document.getElementById("numero8-" + String(x));
+			let numero = document.getElementById("numero8-" + String(x));
 			acumulador += numero.value + " ";
 
 		}
@@ -232,11 +232,11 @@ function dibujarPixeles()
 
 	if (resolucion == "16x16")
 	{
-		var acumulador = "";
+		let acumulador = "";
 
 		for (let x = 0; x < 32; x++) //Son 16 líneas compuestas por 8 + 8 bits cada una
 		{
-			var numero = document.getElementById("numero16-" + String(x));
+			let numero = document.getElementById("numero16-" + String(x));
 			acumulador += numero.value + " ";
 			console.log("acumulador16= <" + acumulador + "> longitud=" + String(acumulador.length));
 		}
@@ -254,16 +254,16 @@ function clicBotonNuevo()
 function clicSelector8()
 { 
 	console.log("disparado selector 8");
-	var numeros = document.getElementById("numeros8");
+	let numeros = document.getElementById("numeros8");
 	numeros.style.display = "flex";
 
 	numeros = document.getElementById("numeros16");
 	numeros.style.display = "none";
 
-	var selector8 = document.getElementById("selector8");
+	let selector8 = document.getElementById("selector8");
 	selector8.className = "selectorAct";
 
-	var selector16 = document.getElementById("selector16");
+	let selector16 = document.getElementById("selector16");
 	selector16.className = "selectorDes";
 
 	resolucion = "8x8";
@@ -272,16 +272,16 @@ function clicSelector8()
 function clicSelector16()
 { 
 	console.log("disparado selector 16");
-	var numeros = document.getElementById("numeros8");
+	let numeros = document.getElementById("numeros8");
 	numeros.style.display = "none";
 
 	numeros = document.getElementById("numeros16");
 	numeros.style.display = "flex";
 
-	var selector8 = document.getElementById("selector8");
+	let selector8 = document.getElementById("selector8");
 	selector8.className = "selectorDes";
 
-	var selector16 = document.getElementById("selector16");
+	let selector16 = document.getElementById("selector16");
 	selector16.className = "selectorAct";
 
 	resolucion = "16x16";
@@ -293,23 +293,87 @@ function clicDibujar()
 	dibujarPixeles();
 }
 
+function ingresarValoresTx(texto, nombreVal, cantidadVal)
+{
+	
+	let listado = texto.split(' ');
+	let posicionVal = 0; //Posición de valor actual que está siendo ingresado
+	let banderaError = false;
+
+
+	for(let l = 0; l < listado.length; l++)
+	{
+		decimal = parseInt(listado[l], 10);
+
+		if (!isNaN(decimal))
+		{
+			let valor = document.getElementById(nombreVal + "-" + String(posicionVal));
+			if (valor != null)
+			{
+				if (decimal >= 0 && decimal <= 255)
+				{
+					valor.value = decimal;
+					++posicionVal;
+				}
+				else
+				{
+					banderaError = true;
+				}
+				
+			}
+			else
+			{
+				valor.style.className = "error";
+			}
+			
+		}
+
+		if (posicionVal > cantidadVal)
+		{
+			window.alert("Ingresó una mayor cantidad de valores de los requeridos!!!");
+			break;
+		}
+	}
+
+
+	if (banderaError)
+	{
+		window.alert("Se hallaron valores no numéricos y fueron omitidos!!!");
+	}
+}
+
+function clicIngresarT()
+{
+	if (resolucion == "8x8")
+	{
+		let texto = window.prompt("Ingrese 8 números decimales (del 0 al 255)", "");
+		ingresarValoresTx(texto, "numero8", 8);
+	}
+
+	if (resolucion == "16x16")
+	{
+		let texto = window.prompt("Ingrese 32 números decimales (del 0 al 255)", "");
+		ingresarValoresTx(texto, "numero16", 32);
+	}
+}
+
 //Al cargarse la ventana //////////////////////////////////////////////////////////
 function inicializar()
 {
 	//Preparado de las grillas
-	var contenedor8 = document.getElementById("contenedor8");
+	let contenedor8 = document.getElementById("contenedor8");
 	iniciarGrilla(contenedor8, 8);
 
-	var contenedor16 = document.getElementById("contenedor16")
+	let contenedor16 = document.getElementById("contenedor16")
 	iniciarGrilla(contenedor16, 16);
 
 	
 	//Preparado de las listas para ingreso de números
-	var listado8C = [];
+	let listado8C = [];
 	listado8C.push(document.getElementById("numeros8-A"));
 	inicializarOpciones("numero8", listado8C, 8);
 
-	var listado16C = [];
+	let listado16C = [];
 	listado16C.push(document.getElementById("numeros16-A"));	
 	listado16C.push(document.getElementById("numeros16-B"));
 	listado16C.push(document.getElementById("numeros16-C"));
@@ -319,6 +383,6 @@ function inicializar()
 	//Preparadon opción por defecto
 	clicSelector8();
 
-	//Preparar las grillas (TODO borrable)
+	//Preparar las grillas
 	cambiarModo("menu"); 	
 }
